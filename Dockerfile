796b5d62a0c928464574c9071890df763e214691
Javeria Lodhi <javeria.lodhi@xloopdigital.com>
2023-05-26 08:16:15 +0500

    files added

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:17
+WORKDIR /app
+ENV PORT 8081
+EXPOSE 8081
+COPY target/*.jar /app/app.jar
+ENTRYPOINT [ "java", "-jar", "app-jar" ]
---
+++ src/main/java/com/cluster/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.cluster.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/say")
+    public String getHello(){
+        return "The Controller is runnning";
+    }
+    
+}
